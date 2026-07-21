17c854a5ced33357f58145d7b0d5e909d863587d
sidharth756 <sidharthrk756@gmail.com>
2026-07-21 22:08:33 +0530

    Add frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div>Credencify</div>
+    </>
+  )
+}
+
+export default App
