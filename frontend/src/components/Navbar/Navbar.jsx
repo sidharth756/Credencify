bb6cfad0a500995c21f3d875efe2874a650b1d5a
sidharth756 <sidharthrk756@gmail.com>
2026-07-27 22:22:58 +0530

    Created Navbar with css

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-
+import './Navbar.css'
 function NavBar() {
   return (
-    <div>
-        <nav>
+    <header className='navbar'>
+      <div className="logo">
+          <h2>Credencify</h2>
+
+      </div>
+
+        <nav className='nav-links'>
             <a href="">Home</a>
             <a href="">Contact</a>
             <a href="">Verify Credential</a>
             <a href="">About us</a>
             <a href="">Sign In</a>
         </nav>
+        <button className='signin-btn'>Sign In</button>
 
-    </div>
+    </header>
   )
 }
 
