e9a7490186d2946599abe7a7cd0cd1908903fcba
sidharth756 <sidharthrk756@gmail.com>
2026-07-27 22:22:58 +0530

    Created Navbar with css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import NavBar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <div>Credencify</div>
     </>
   )
 }
