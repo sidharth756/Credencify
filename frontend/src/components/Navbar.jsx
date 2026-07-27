2738cc469da23d082115fc1e59d2692856a64e21
sidharth756 <sidharthrk756@gmail.com>
2026-07-27 20:36:43 +0530

    Added component folder

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Contact</a>
+            <a href="">Verify Credential</a>
+            <a href="">About us</a>
+            <a href="">Sign In</a>
+        </nav>
+
+    </div>
+  )
+}
+
+export default NavBar
