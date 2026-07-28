db27ef363be99f08e63d6ec7a19684bd76d91196
Shebha-M <shebham.06@gmail.com>
2026-07-28 21:34:00 +0530

    added Navbar into separate folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import SignIn from './pages/SignIn/SignIn'
 import Welcome from './pages/Welcome/Welcome'
 
 function App() {
   return (
     <>
       <Welcome />
+      <SignIn />  
     </>
   )
 }
