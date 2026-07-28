710ba6ddbb5ddefd6215a19326a38b7ba8124017
sidharth756 <sidharthrk756@gmail.com>
2026-07-28 21:40:27 +0530

    Fixed the scroll and basic global responsiveness

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -15,24 +15,4 @@ function Welcome() {
     </>
   );
 }
-
-export default Welcome;
-import HeroSection from "../../components/HeroSection";
-import LoginCard from "../../components/LoginCard";
-import Navbar from "../../components/Navbar";
-
-import styles from "./Welcome.module.css";
-
-function Welcome() {
-  return (
-    <>
-      <Navbar />
-      <main className={styles.container}>
-        <HeroSection />
-        <LoginCard />
-      </main>
-    </>
-  );
-}
-
 export default Welcome;
