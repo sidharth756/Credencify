179527b2fc98d091529a0d2100bc8c3fa34ab9ad
Shebha-M <shebham.06@gmail.com>
2026-07-29 17:36:55 +0530

    Update Register import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import HeroSection from './components/HeroSection/HeroSection'
 import LoginCard from './components/LoginCard/LoginCard'
 import NavBar from "./components/Navbar/Navbar";
 import Welcome from './pages/Welcome/Welcome'
-import Register from './components/Register/Register'
+import Register from './pages/Register/Register'
 
 function App() {
   return (
-    <> <Register />;    </>
+    <> <Register />   </>
   )
 }
 
