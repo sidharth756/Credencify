71ec791e2a221d631dc7d346493c507431e612cb
rujitha-g <rujitha42@gmail.com>
2026-08-04 15:40:11 +0530

    Added lastname label

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -3,9 +3,10 @@ import styles from "./Contact.module.css";
 
 function Contact() {
   const [firstName, setFirstName] = useState("");
-
+  const [lastName, setLastName] = useState("");
   return (
     <div className={styles.contact}>
+     
       <label For="firstName">First Name</label>
 
       <input
@@ -15,6 +16,18 @@ function Contact() {
         value={firstName}
         onChange={(e) => setFirstName(e.target.value)}
       />
+
+       <label For="lastName">Last Name</label>
+
+      <input
+        id="lastName"
+        type="text"
+        placeholder="Enter your last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+
+
     </div>
   );
 }
